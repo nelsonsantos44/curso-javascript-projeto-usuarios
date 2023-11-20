8a934d69dcbe57978908192de86ce7d04c1ffaf9
nelsonsantos44 <rodrigues.nelson1333@gmail.com>
2023-11-19 21:52:47 -0300

    atualizando

---
+++ index.js
@@ -0,0 +1,25 @@
+var fields = document.querySelectorAll ("#form-user-create [name]");
+
+var user = {};
+
+fields.forEach(function(field, index){
+
+    if (field.name == "gender") {
+               
+        if (field.checked){
+
+            user[field.name] = field.value
+        }
+
+    } else {
+
+        user[field.name] = field.value;
+
+    }
+
+    //console.log(field.id, field.name, field.value , field.checked , field.index)
+
+});
+
+
+console.log (user)
